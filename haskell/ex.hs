2078c27b12633c3683ff1044d02aa96b3ad34c24
Iago Floriano <iagomfloriano@gmail.com>
2022-08-08 00:47:35 -0300

    Usei hindent

--- haskell/ex.hs
+++ haskell/ex.hs
@@ -2,22 +2,22 @@ import System.IO
 
 -- ex 1
 inverte :: Integer -> Integer
-inverte x 
-  | x < 10   = x
+inverte x
+  | x < 10 = x
   | otherwise = (10 ^ casas) * mod x 10 + inverte (div x 10)
   where
-  casas = floor ( logBase 10 (fromInteger x))
+    casas = floor (logBase 10 (fromInteger x))
 
 -- ex 2
 somaVetor :: [Integer] -> Integer
 somaVetor [] = 0
 somaVetor [x] = x
-somaVetor (x:xs) = x + somaVetor(xs)
+somaVetor (x:xs) = x + somaVetor (xs)
 
 -- ex 3
 somatorio :: Integer -> Integer
 somatorio 0 = 0
-somatorio x = x + somatorio (x-1)
+somatorio x = x + somatorio (x - 1)
 
 -- ex 4
 inverteReais :: [Float] -> [Float]
@@ -31,50 +31,52 @@ conta _ 0 = 0
 conta 0 _ = 0
 conta k n
   | mod n 10 == k = conta k (div n 10) + 1
-  | otherwise     = conta k (div n 10)
+  | otherwise = conta k (div n 10)
 
 -- ex 6
 multipRec :: Integer -> Integer -> Integer
 multipRec _ 0 = 0
 multipRec 0 _ = 0
-multipRec n1 n2 = n1 + (multipRec n1 (n2-1))
+multipRec n1 n2 = n1 + (multipRec n1 (n2 - 1))
 
 -- ex 7
 printNaturaisC :: Integer -> String
 printNaturaisC 0 = "0"
-printNaturaisC n = printNaturaisC (n-1)  ++ "\n" ++ (show n)
+printNaturaisC n = printNaturaisC (n - 1) ++ "\n" ++ (show n)
 
 -- ex 8
 printNaturaisD :: Integer -> String
 printNaturaisD 0 = "0"
-printNaturaisD n = (show n) ++ "\n" ++ printNaturaisD (n-1)  
+printNaturaisD n = (show n) ++ "\n" ++ printNaturaisD (n - 1)
 
 -- ex 9
 paresAteC :: Integer -> String
 paresAteC n
   | n <= 0 = show n
-  | otherwise = paresAteC (n-2) ++ "\n" ++ (show n)
+  | otherwise = paresAteC (n - 2) ++ "\n" ++ (show n)
 
 -- ex 10
 paresAteD :: Integer -> String
 paresAteD n
   | n <= 0 = show n
-  | otherwise = (show n) ++ "\n" ++ paresAteD (n-2)
+  | otherwise = (show n) ++ "\n" ++ paresAteD (n - 2)
 
 -- ex 11
 fatDuplo :: Integer -> Integer
 fatDuplo n
   | n <= 1 = 1
-  | otherwise = n * fatDuplo (n-2)
+  | otherwise = n * fatDuplo (n - 2)
 
 --ex 12
 fatorial :: Integer -> Integer
-fatorial n | n <= 1 = 1
-fatorial n = n * fatorial (n-1)
+fatorial n
+  | n <= 1 = 1
+  | otherwise = n * fatorial (n - 1)
 
 superFatorial :: Integer -> Integer
-superFatorial n | n <= 1 = 1
-superFatorial n = (fatorial n) * superFatorial(n-1)
+superFatorial n
+  | n <= 1 = 1
+  | otherwise = (fatorial n) * superFatorial (n - 1)
 
 --ex 13
 menorVetor :: [Integer] -> Integer
@@ -86,31 +88,33 @@ menorVetor (x:xs)
 
 --ex 14
 imprimeSerie :: Integer -> Integer -> Integer -> String
-imprimeSerie _ _ 0 = ""
-imprimeSerie i j _ | i > j = ""
-imprimeSerie i j k | i + k >= j = show i --ultimo numero nao ter \n
-imprimeSerie i j k = show i ++ "\n" ++ (imprimeSerie newi j k)
+imprimeSerie i j k
+  | k == 0 = ""
+  | i > j = ""
+  | i + k >= j = show i --ultimo numero nao ter \n
+  | otherwise = show i ++ "\n" ++ (imprimeSerie newi j k)
   where
-  newi = i + k
+    newi = i + k
 
 --ex 15
 binario :: Integer -> String
 binario x
-  | x < 2     = j
+  | x < 2 = j
   | otherwise = j ++ (binario (div x 2))
   where
-  j = show $ mod (x+1) 2
+    j = show $ mod (x + 1) 2
 
 --ex 16
 somaRec :: Integer -> Integer -> Integer
 somaRec x 0 = x
 somaRec 0 x = x
-somaRec x y = somaRec x (y-1) + 1
+somaRec x y = somaRec x (y - 1) + 1
 
 --ex 17
 somaDig :: Integer -> Integer
-somaDig n | n < 10 = n
-somaDig n = (mod n 10) + somaDig (div n 10)
+somaDig n
+  | n < 10 = n
+  | otherwise = (mod n 10) + somaDig (div n 10)
 
 main = do
   putStrLn "------------------\nComeço do programa\n"
