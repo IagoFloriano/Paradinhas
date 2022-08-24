b6cd7b0e3ffe0ab7d9ba6b009d56101062ca3f8b
Iago Floriano <iagomfloriano@gmail.com>
2022-08-24 16:29:33 -0300

    Rotação esquerda e direita

--- haskell/aulaTipos.hs
+++ haskell/aulaTipos.hs
@@ -6,7 +6,7 @@ data ArvoreBin
 arvoreBin =
   NodoInt
     4
-    (NodoInt 2 NodoNull NodoNull)
+    (NodoInt 2 NodoNull (NodoInt 3 NodoNull NodoNull))
     (NodoInt 10 (NodoInt 5 NodoNull NodoNull) (NodoInt 15 NodoNull NodoNull))
 
 passeiaBin :: ArvoreBin -> [Int]
@@ -18,3 +18,15 @@ insere NodoNull n = NodoInt n NodoNull NodoNull
 insere (NodoInt x e d) n
   | n <= x = (NodoInt x (insere e n) d)
   | otherwise = (NodoInt x e (insere d n))
+
+rotD :: ArvoreBin -> ArvoreBin
+rotD NodoNull = NodoNull
+rotD (NodoInt x e d) = (NodoInt n ee (NodoInt x ed d))
+  where
+    (NodoInt n ee ed) = e
+
+rotE :: ArvoreBin -> ArvoreBin
+rotE NodoNull = NodoNull
+rotE (NodoInt x e d) = (NodoInt n (NodoInt x e de) dd)
+  where
+    (NodoInt n de dd) = d
